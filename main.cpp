845c0ddb0996ecb1b21f28cfa371973e1660c120
mxchen2001 <53723151+mxchen2001@users.noreply.github.com>
2020-05-01 14:08:02 -0500

    player Class implemented

--- main.cpp
+++ main.cpp
@@ -60,26 +60,87 @@
 #include "UART.h"
 #include "Timer0.h"
 #include "Timer1.h"
-
+#include <string>
 
 SlidePot sensor(373, 21); // initialize sensor 
 uint32_t Data;      // 12-bit ADC
-uint32_t Position;  // 32-bit fixed-point 0.01 cm
+uint32_t Position = 10;  // 32-bit fixed-point 0.01 cm
 
 extern "C" void DisableInterrupts(void);
 extern "C" void EnableInterrupts(void);
 extern "C" void SysTick_Handler(void);
 
 // Creating a struct for the Sprite.
-typedef enum {dead,alive} status_t;
+void movingDino();
+void jumpingDino();
+
+int flag;
+int duckFlag;
+int jumpFlag;
+
 struct sprite{
-  uint32_t x;      // x coordinate
-  uint32_t y;      // y coordinate
-  const unsigned short *image; // ptr->image
+	const unsigned short *image; // ptr->image
   uint32_t width;
 	uint32_t height;
 };          
 typedef struct sprite sprite_t;
+sprite_t dino = {TRexStationary,20,20};
+sprite_t dinoLeft = {TRexLeft,20,20};
+sprite_t dinoRight = {TRexRight,20,20};
+sprite_t dinoBlank = {blankStand, 20, 20};
+sprite_t dinoLeftDuck = {TRexDuckLeft, 26, 13};
+sprite_t dinoRightDuck = {TRexDuckRight,26, 13};
+sprite_t dinoBlankDuck = {blankDuck, 26, 13};
+sprite_t tallCact = {tallCactus, 20, 24};
+sprite_t tallCactBlank = {blankTall, 20, 24};
+
+struct Player {
+	uint32_t x;      // x coordinate
+  uint32_t y;      // y coordinate
+	int32_t vx;
+	int32_t vy;
+	sprite_t* pSprite;
+	sprite_t* blankSprite;
+public:
+	Player(uint32_t ix, uint32_t iy, sprite_t* nSprite, sprite_t* nbSprite, int32_t nvx, int32_t nvy)
+	: x(ix), y(iy), vx(nvx), vy(nvy),  pSprite(nSprite) ,  blankSprite(nbSprite) {}
+		
+	void move(int dx, int dy){
+		this->x += dx;
+		this->y += dy;
+	}
+	
+	void drawPlayer(){
+		ST7735_DrawBitmap(x, y, pSprite->image, pSprite->width, pSprite->height);
+	}
+	
+	void clearPlayer(){
+		ST7735_DrawBitmap(x, y, blankSprite->image, blankSprite->width, blankSprite->height);
+	}
+	
+	
+	void movePlayer(int32_t nvx, int32_t nvy){
+		vx = nvx;
+		vy = nvy;
+	}
+	
+	
+		
+	friend void movingDino();
+	
+	friend void jumpingDino();
+	
+};
+
+
+Player TRex(20, 120, &dino, &dinoBlank, 0, 0);
+Player Cactus(128, 120, &tallCact, &tallCactBlank, 4, 0);
+
+
+class Projectile{
+	uint32_t velocity;
+	uint32_t damage;
+};
 
 void SysTick_Init(void){
   NVIC_ST_CTRL_R = 0;                   // disable SysTick during setup
@@ -106,34 +167,95 @@ void IO_Init(void) {
 	GPIO_PORTF_PDR_R |= 0x10;
 }
 
-sprite_t dinoLeft = {20,120,TRexLeft,20,20};
-sprite_t dinoRight = {20,120,TRexRight,20,20};
-sprite_t tallCact = {50, 50, tallCactus, 20, 24};
+int jumpSize[] = {0, 1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 1, 0};  // 14
 
-sprite_t* mainSprite = &dinoLeft;
-
-// sprite_t mainScreen = {0, 0, 
-
-
-int flag;
-int jumpFlag;
-int jumpSize[] = {4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 9, 9, 9, 9, 8, 8, 8, 8, 7, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 5, 4, 4, 4, 4};
 
 void movingDino(){
-	if(flag){
-		mainSprite = &dinoLeft;
-		flag = 0;
+	if(!jumpFlag){
+		//TRex.clearPlayer();
+		static int counter = 0;
+		if(counter == 0){
+			if(duckFlag){
+				GPIO_PORTF_DATA_R &= 0x00;
+				GPIO_PORTF_DATA_R ^= 0x04;
+				if(flag){
+					TRex.pSprite = &dinoLeftDuck;
+					flag = 0;
+				} else {
+					TRex.pSprite = &dinoRightDuck;
+					flag = 1;
+				}
+			} else {
+				GPIO_PORTF_DATA_R &= 0x00;
+				GPIO_PORTF_DATA_R ^= 0x08;
+				if(flag){
+					TRex.pSprite = &dinoLeft;
+					flag = 0;
+				} else {
+					TRex.pSprite = &dinoRight;
+					flag = 1;
+				}
+			}
+		} else if(counter >= 10000){
+			counter = 0;
+		}	else {
+			counter++;
+		}
+		//TRex.clearPlayer();
 	} else {
-		mainSprite = &dinoRight;
-		flag = 1;
+		static int jumpComplete = 0;
+		static int enterJump = 0;
+		static int index = 0;
+
+		if(!enterJump){
+			TRex.pSprite = &dino;
+			jumpComplete = 1;
+			enterJump = 1;
+		}
+		GPIO_PORTF_DATA_R &= 0x00;
+		GPIO_PORTF_DATA_R ^= 0x02;
+			//TRex.clearPlayer();
+		TRex.y = 120 - jumpSize[index] * 6;
+		index++;
+
+		TRex.clearPlayer();
+		/*** exit case***/
+		if(index == 14){
+			jumpComplete = 0;
+			index = 0;
+			enterJump = 0;
+			jumpFlag = 0;
+		}
+	}		
+	
+	
+	Cactus.clearPlayer();
+	if(Cactus.x > 0){
+		Cactus.x -= Cactus.vx / 2;
+	} else {
+		Cactus.x = 128;
+	}
+	Cactus.clearPlayer();
+}
+
+void checkPosition(){
+	if(Position > 120){
+		duckFlag = 1;
+	} else if(Position < 40){
+		jumpFlag = 1;
+		duckFlag = 0;
+	} else {
+		duckFlag = 0;
 	}
 }
+
 
 int main(void){
   PLL_Init(Bus80MHz);       // Bus clock is 80 MHz 
   Random_Init(1);
   Output_Init();
-  Timer0_Init(&movingDino,16000000); // 50 Hz
+  Timer0_Init(&movingDino,1000000); // 50 Hz
+	// Timer1_Init(&screenRefresh,8000000); // 50 Hz
 	SysTick_Init();
 	PortF_Init();
 	IO_Init();
@@ -141,52 +263,25 @@ int main(void){
 	EnableInterrupts();
 	
   ST7735_FillScreen(0x0000);            // set screen to black
-	int previousX, previousY, preYCoord;
-	int index = 0;
+	int previousY, preYCoord;
   while(1){
-		sensor.Sync(); // wait for semaphore
-		previousX = Position;
-		previousY = mainSprite->y;
-		Data = sensor.ADCsample(); // sample 12-bit channel 5
+		sensor.Sync();
+		Data = sensor.ADCsample();
     Position = sensor.Distance(); Position /= 5; Position *= 5;
+
+		
+		
 		if((GPIO_PORTF_DATA_R & 0x10) == 0x10){
-				jumpFlag = 1;
+				// jumpFlag = 1;
 		}
-		if(previousX > Position){
-			ST7735_DrawBitmap(previousX, mainSprite->y, blank, mainSprite->width, mainSprite->height);
-		} else if(previousX < Position) {
-			ST7735_DrawBitmap(previousX , mainSprite->y, blank, mainSprite->width, mainSprite->height);
-		} else if(previousY != mainSprite->y){
-			ST7735_DrawBitmap(mainSprite->x, previousY, blank, mainSprite->width / 2, mainSprite->height);
-		}
-		
-    //ST7735_SetCursor(0,0);
-		mainSprite->x = Position;
-		if(!jumpFlag){
-			ST7735_DrawBitmap(mainSprite->x, mainSprite->y, mainSprite->image, mainSprite->width, mainSprite->height);
-			preYCoord = mainSprite->y;
-			index = 0;
-		} else {
-			int yCoord = mainSprite->y - (4 * jumpSize[index]);
-			ST7735_DrawBitmap(mainSprite->x, preYCoord, blank, mainSprite->width, mainSprite->height);
-			ST7735_DrawBitmap(mainSprite->x, yCoord, mainSprite->image, mainSprite->width, mainSprite->height);
-			index++;
-			preYCoord = mainSprite->y - (4 * jumpSize[index - 1]);
-		}
-		
-		if(index == 56){
-			ST7735_DrawBitmap(mainSprite->x, preYCoord, blank, mainSprite->width, mainSprite->height);
-			index = 0;
-			jumpFlag = 0;
-		}
-		ST7735_DrawBitmap(tallCact.x, tallCact.y, tallCact.image, tallCact.width, tallCact.height);
-
-  }
-
+		checkPosition();
+		TRex.drawPlayer();
+		Cactus.drawPlayer();
 }
+	}
+
 
 void SysTick_Handler(void){ // every sample
-	GPIO_PORTF_DATA_R ^= 0x0E;
   sensor.Save(ADC_In());
 }
 
