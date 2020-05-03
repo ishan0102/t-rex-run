bc935d5f2c706469ca4ba8371d9c0919a9704d31
Ishan Shah <ishan0102@gmail.com>
2020-05-03 15:28:45 -0500

    Make separate player class

--- Player.h
+++ Player.h
@@ -17,52 +17,49 @@ struct sprite{
 	uint32_t height;
 };          
 typedef struct sprite sprite_t;
-sprite_t dino = {TRexStationary,20,20};
-sprite_t dinoLeft = {TRexLeft,20,20};
-sprite_t dinoRight = {TRexRight,20,20};
-sprite_t dinoBlank = {blankStand, 20, 20};
-sprite_t dinoLeftDuck = {TRexDuckLeft, 26, 13};
-sprite_t dinoRightDuck = {TRexDuckRight,26, 13};
-sprite_t dinoBlankDuck = {blankDuck, 26, 13};
-sprite_t tallCact = {tallCactus, 20, 24};
-sprite_t tallCactBlank = {blankTall, 20, 24};
+sprite_t dino = {TRexStationary, 30, 24};
+sprite_t dinoLeft = {TRexLeft, 30, 24};
+sprite_t dinoRight = {TRexRight, 30, 24};
+sprite_t dinoBlank = {blankStand, 30, 24};
+sprite_t dinoLeftDuck = {TRexDuckLeft, 30, 24};
+sprite_t dinoRightDuck = {TRexDuckRight, 30, 24};
+sprite_t dinoBlankDuck = {blankDuck, 30, 17};
+sprite_t tallCact = {tallCactus, 26, 28};
+sprite_t tallCactBlank = {blankTall, 26, 28};
 
 struct Player {
-	uint32_t x;      // x coordinate
-  uint32_t y;      // y coordinate
+	int32_t x;      // x coordinate
+  int32_t y;      // y coordinate
 	int32_t vx;
 	int32_t vy;
 	sprite_t* pSprite;
 	sprite_t* blankSprite;
 public:
-	Player(uint32_t ix, uint32_t iy, sprite_t* nSprite, sprite_t* nbSprite, int32_t nvx, int32_t nvy)
+	Player(int32_t ix, int32_t iy, sprite_t* nSprite, sprite_t* nbSprite, int32_t nvx, int32_t nvy)
 	: x(ix), y(iy), vx(nvx), vy(nvy),  pSprite(nSprite) ,  blankSprite(nbSprite) {}
-		
+
 	void move(int dx, int dy){
 		this->x += dx;
 		this->y += dy;
 	}
-	
+
 	void drawPlayer(){
 		ST7735_DrawBitmap(x, y, pSprite->image, pSprite->width, pSprite->height);
 	}
-	
+
 	void clearPlayer(){
 		ST7735_DrawBitmap(x, y, blankSprite->image, blankSprite->width, blankSprite->height);
 	}
-	
-	
+
+
 	void movePlayer(int32_t nvx, int32_t nvy){
 		vx = nvx;
 		vy = nvy;
 	}
-	
-	
-		
+
+
+
 	friend void movingDino();
-	
-	friend void jumpingDino();
-	
 };
 
 Player TRex(20, 120, &dino, &dinoBlank, 0, 0);
@@ -73,11 +70,11 @@ class Projectile{
 	uint32_t damage;
 };
 
-int jumpSize[] = {0, 1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 1, 0};  // 14
+int jumpSize[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 6, 5, 4, 3, 2, 1, 0};  // 14
 
 void movingDino(){
 	if(!jumpFlag){
-		//TRex.clearPlayer();
+		// TRex.clearPlayer();
 		static int counter = 0;
 		if(counter == 0){
 			if(duckFlag){
@@ -107,6 +104,7 @@ void movingDino(){
 			counter++;
 		}
 		//TRex.clearPlayer();
+
 	} else {
 		static int jumpComplete = 0;
 		static int enterJump = 0;
@@ -119,27 +117,27 @@ void movingDino(){
 		}
 		GPIO_PORTF_DATA_R &= 0x00;
 		GPIO_PORTF_DATA_R ^= 0x02;
-			//TRex.clearPlayer();
+		TRex.clearPlayer();
 		TRex.y = 120 - jumpSize[index] * 6;
 		index++;
 
 		TRex.clearPlayer();
 		/*** exit case***/
-		if(index == 14){
+		if(index == 18){
 			jumpComplete = 0;
 			index = 0;
 			enterJump = 0;
 			jumpFlag = 0;
 		}
 	}		
-	
-	Cactus.clearPlayer();
-	if(Cactus.x > 0){
-		Cactus.x -= Cactus.vx / 2;
+
+
+
+	if(Cactus.x > -22){
+		Cactus.x -= Cactus.vx;
 	} else {
 		Cactus.x = 128;
 	}
-	Cactus.clearPlayer();
 }
 
 uint32_t Position = 10;  // 32-bit fixed-point 0.01 cm
